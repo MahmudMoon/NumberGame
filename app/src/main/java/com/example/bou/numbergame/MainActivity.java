572ef608d7945a0949092dcf28831dd60e983b9d
moon <mooncseru14@gmail.com>
2018-05-23 02:52:07 +0600

    Completed the ui of activity_main.xml

--- app/src/main/java/com/example/bou/numbergame/MainActivity.java
+++ app/src/main/java/com/example/bou/numbergame/MainActivity.java
@@ -3,15 +3,23 @@ package com.example.bou.numbergame;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import java.nio.Buffer;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     Button start;
+    CardView cardView;
+    Button setting;
+    Button save;
+    Button cancel;
+    CheckBox music_play,setVibreation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +39,29 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        cardView.setVisibility(View.INVISIBLE);
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardView.setVisibility(View.VISIBLE);
+
+            }
+        });
     }
 
     private void init_variabes() {
 
+
     }
 
     private void init_views() {
-        start = (Button)findViewById(R.id.btn_start);
+        start = (Button) findViewById(R.id.btn_start);
+        cardView = (CardView) findViewById(R.id.cardView);
+        setting = (Button) findViewById(R.id.btn_settings);
+        save = (Button) findViewById(R.id.btn_save);
+        cancel = (Button)findViewById(R.id.btn_cancel);
+        music_play = (CheckBox)findViewById(R.id.chb_music);
+        setVibreation = (CheckBox)findViewById(R.id.chb_vibration);
     }
 }
